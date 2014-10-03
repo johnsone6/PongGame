f08c3a0eb88cee7de2943a5df780e55047b74835
johnsone6 <johnsone6@rowan.edu>
2014-10-03 10:25:55 -0400

    scores.java

---
+++ scores.java
@@ -0,0 +1,57 @@
+public class Scores
+{
+    // instance variables - replace the example below with your own
+    private int points;
+    private int wins;
+   
+
+    /**
+     * Constructor for objects of class Scores
+     */
+    public Scores()
+    {
+        // initialise instance variables
+
+    }
+
+    /**
+    
+     */
+    public void setWins(int s)
+    {
+        
+    }
+    public int getWins()
+    {
+        return wins;
+    }
+    public void setPoints(int p)
+    {
+        
+    }
+    public int getPoints()
+    {
+        return points;
+    }
+    public void countPoint()
+    {
+        
+    }
+    public void countWins()
+    {
+        
+    }
+    public void addWins()
+    {
+        
+    }
+    public void sortScores()
+    {
+        
+    }
+    public void printHighScores()
+    {
+        
+    }
+    
+}
