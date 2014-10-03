b1f1fcaab34e253ded24e60c32992661cd9de49d
johnsone6 <johnsone6@rowan.edu>
2014-10-03 10:26:42 -0400

    Player.java

---
+++ Player.java
@@ -0,0 +1,49 @@
+public class Player
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private int numberOfHits;
+    private int score;
+    
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+        
+
+    }
+
+    /**
+     *  
+     */
+    public void setName(String n)
+    {
+        name = n;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setScore(int s)
+    {
+        score = s;
+    }
+    public int getScore()
+    {
+        return score;
+    }
+   /* public void updateMove()
+    {
+    }*/
+    
+    /*
+     * When a player gets the ball passed the other player, the player gains a point
+     */
+    public void countPoint()
+    {
+        
+    }
+    
+}
